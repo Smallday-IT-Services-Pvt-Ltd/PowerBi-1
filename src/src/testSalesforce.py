87713c71b83ec0b20a1cfd3145884e2d182a2471
Afshanbanu89 <afshan@smalldaytech.com>
2020-11-09 16:13:26 +0530

    changes done

--- src/src/testSalesforce.py
+++ src/src/testSalesforce.py
@@ -4,8 +4,9 @@ username = os.getenv("username")
 password=os.getenv("password")
 security_token = os.getenv("security_token")
 grant_type=os.getenv("grant_type")
-client_id=os.getenv("client_id")
 client_secret=os.getenv("client_secret")
+client_id=os.getenv("client_id")
+
 
 params = {
     "grant_type": "password",
